5f018725bc2edebe76bdb6d891de0b508efdf3d6
Aaron Feldman <feldmando@gmail.com>
2015-12-29 16:51:31 -0800

    Saves message
    Saves message to documents/mytestdir/mytestfile

---
+++ app/src/main/java/com/example/aaron/testapplication/DisplayMessageActivity.java
@@ -0,0 +1,66 @@
+package com.example.aaron.testapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FileOutputStream outputStream;
+        try {
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state)) {
+                File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "mytestdir");
+
+                dir.mkdirs();
+
+                File file = new File(dir, "mytestfile");
+
+                Intent intent = getIntent();
+                String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+                outputStream = new FileOutputStream(file);
+                outputStream.write(message.getBytes());
+                outputStream.close();
+
+                TextView textView = new TextView(this);
+                textView.setTextSize(40);
+                textView.setText(message);
+                RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+                layout.addView(textView);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
